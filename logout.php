7ab097d56e8c3f685db35a9485e0f5a0474aa06a
alba-mu <albamu2002@gmail.com>
2025-11-07 17:09:00 +0100

    First commit

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+if (isset($_SESSION['role'])) {  //session exists
+    session_destroy();
+    header("Location: index.php");    
+} else {
+    //TODO: decide how to treat this case.
+    //echo "<p>You must login before logout</p>";
+    //header("Location: index.php");    
+}
