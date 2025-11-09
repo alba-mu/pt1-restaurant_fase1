ed7016d9192501d508087a1334b2d81dcde35940
alba-mu <albamu2002@gmail.com>
2025-11-09 17:30:53 +0100

    Document and format code

--- adminusers.php
+++ adminusers.php
@@ -2,24 +2,35 @@
 session_start();
 $current_page = 'adminusers.php';
 require_once './fn-php/fn-roles.php';
-include_once "topmenu.php";
 ?>
 
+<!--Barra de navegació-->
+<?php include_once "topmenu.php"; ?>
+
+<!-- Contingut de la pàgina -->
 <main class="flex-grow-1 container py-4">
     <div class="container-fluid">
 
         <div class="container">
             <h2>Admin users</h2>
-            <?php if (isGranted($_SESSION['role']??'', 'adminusers')): ?> 
+            <?php if (isGranted($_SESSION['role'] ?? '', 'adminusers')): ?>
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum
                 </p>
-        <?php else: ?> 
+            <?php else: ?>
                 <p>You are not allowed to acces this page!</p>
-        <?php endif; ?>
+            <?php endif; ?>
         </div>
     </div>
 </main>
-<?php include_once "footer.php";?>
+
+<!-- Footer -->
+<?php include_once "footer.php"; ?>
+
 </body>
+
 </html>
--- daymenu.php
+++ daymenu.php
@@ -1,19 +1,30 @@
 <?php
 session_start();
 $current_page = 'daymenu.php';
-include_once "topmenu.php";
 ?>
 
+<!--Barra de navegació-->
+<?php include_once "topmenu.php"; ?>
+
+<!-- Contingut de la pàgina -->
 <main class="flex-grow-1 container py-4">
-<div class="container-fluid">
-    <div class="container">
-        <h2>Day menu</h2>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-        </p>
+    <div class="container-fluid">
+        <div class="container">
+            <h2>Day menu</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum
+            </p>
+        </div>
     </div>
-</div>
 </main>
-<?php include_once "footer.php";?>
+
+<!-- Footer -->
+<?php include_once "footer.php"; ?>
+
 </body>
+
 </html>
